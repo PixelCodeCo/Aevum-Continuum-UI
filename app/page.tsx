8d74b412a19c25f631c3dcd55c0e92fbe82336b1
Jack Tubby <jack200034@hotmail.com>
2026-01-06 20:14:38 +0000

    Hide footer for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import { Timeline } from "@/app/components/Timeline";
 import { SAMPLE_EVENTS } from "@/app/constants/timeline";
-import { Footer } from "./components/Footer";
 
 export default function Home() {
   return (
     <>
       <Timeline events={SAMPLE_EVENTS} />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
