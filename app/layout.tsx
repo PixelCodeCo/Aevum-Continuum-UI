5e6739d361e7d4ed46dd9a1202de1f3fac7df304
Jack Tubby <jack200034@hotmail.com>
2026-01-08 21:19:10 +0000

    Add Analytics component and update package dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/app/components/Navbar";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +32,8 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
-        <SpeedInsights/>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
